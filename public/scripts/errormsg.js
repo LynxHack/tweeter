73651fdee436332fa4a053aa5d28ad4ce0610d50
klynx <KevinLynx_L@hotmail.com>
2018-06-20 09:44:22 -0700

    Added dynamic tweet adding

--- public/scripts/errormsg.js
+++ public/scripts/errormsg.js
@@ -21,12 +21,53 @@ $(function() {
         document.querySelector(".tweet-error").style.border = "0";
         $.ajax({url: '/tweets', data: $(this).serialize(), type: 'POST',
             success: function(responseData, textStatus, jqXHR) {
-                console.log("response: ", responseData);
+                console.log("response: ", responseData);      
+                loadTweets();
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.log(errorThrown);
             }
         });
+      }
+
+      function createTweetElement(tweet) {
+        let $test = 
+          `<section class="post-tweet">
+          <img src="${tweet.user.avatars.regular}"  alt="avatar">
+          <h2>${tweet.user.name}</h2>
+          <span class="aturl">${tweet.user.handle}</span>
+          <section class="post-content">${tweet.content.text}</section>
+          <hr>
+          <footer>
+            ${tweet.created_at}
+            <span class="tweeticons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+            </span>
+          </footer>
+          </section>`;
+        let $tweet = $('<article>').addClass('.post-tweet');
+        $tweet.text(tweet.content.text);
+        return $test;
+      }
+  
+      function renderTweets(tweets) {
+        $('.Tweet-container').html("");
+        for(element in tweets){
+            $('.Tweet-container').prepend(createTweetElement(tweets[element]));
+        }
+      }
+      
+      function loadTweets(){
+        $.ajax({url: "/tweets", type: "GET", 
+        success: function(data) {
+          console.log(data);
+          renderTweets(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }});
       }
     });
   });
